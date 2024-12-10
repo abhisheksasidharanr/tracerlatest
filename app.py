8422b780ec81372bc0619a99452aeaffc61d7c9e
abhisheksasidharanr <abhisheksasidharanr@gmail.com>
2024-12-11 00:50:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,30 +9,20 @@ app = Flask(__name__)
 def initialize_earth_engine():
     try:
         # Retrieve the service account credentials from the environment variable
-        service_account_json = r'''{
-  "type": "service_account",
-  "project_id": "ee-abhisheksasidharanr",
-  "private_key_id": "9332dd99f683a6d41ea162c051c52627e5d75463",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCvYFcC11Ivgw0E\nhuF1HVH0EgsVOvdzMaR0F0OLXvNxzaAl/MgSVH+JbZAKt8ptfWvbd5z41UHen3zv\nbB1afr61h/U+5p5gWtpyDkfIVJDDGyadQQwvMJ0Qc5eOUlDG/7263tfRfcB7q9f9\nnP1QTJkXVpLiG54VXMNTk4++cq+b1Lklf0nP38XJZ+0TAfRs09+Htk4Yh/gc0NPo\n8Gd6sYeNK3Q1dU5fLyrAT+AUswif+LHTjbnEQrvX2DgQRsIDCwJLI/RCrvFNWAif\nauZOMpiFsIFe0rfkqFOUS8J5R6H0JsYS12Kf7dNy8ocBJKoyAisGI19VpeXcCLIL\nVLbCFMtnAgMBAAECggEAFZfPOXZ+ftgWvGXJEQI9AB1P8ltncpj8TnOQ1EeqwhUs\ncJDuUgdyXomNZdcZnEqILaZNaaaEQjSxOB9qgSUVi8SuJwJTzLmoUh5nqzGc/ftz\n+9exotvMSDrF6yGVIt4DznmNPr90zs//5juJRZ+bbTAHzMF+wNLs6neYMr2XLqCZ\nYgIHIU1br57+6wOrL9lre26AC8L6jUMVtHe+W49OG4nJimgExAxGYUGR0EcHo/FP\nnOW90QbPcnq2krPz81Dza1AmwpwGMfGqbrV7cgF8+f3TJUuTUumWCWbbA3VMebk1\nZfov9P/4nz6ACkgqOFnP5ixkX2x4uGxNNJsVv/rEjQKBgQDvUaXnnVvhl5Hx3LBg\ne66ysuXUTTmmorKd6zRlsr/x2SGotBX466Fyca6gwtMPTh9tVQyocNmGYjRbf32K\ns8esw1Xar8HgmDlGiRp+mrFdgSDJEQELaXoL3wlaTPLPdA+MhGGSVwaMCe/Ofx4w\n2Spz3X1kjtBL7QoPHSG4QAX/8wKBgQC7mbbyPMdiBlblv9olN/w+ZW0MUPni36LM\niIJyOnvzfUspyxXPLkjVzO0co03Lh6LJl73F+AFUQYk991VOfhk8uSLlJOswZwl6\nur2UM8C22AkPpXWmSi2RYXxeABs7TNvC/zXs5++j3NZZPha5CTBezED9a8q1o4S+\nAYmRT7gXvQKBgQDqg2r8/aGU/i9g/+7CjVDTAiXfldFhrls0DtEouzIGr8yhAd/q\nLhTmDeqe1Gt4uwhm3gnqYbN3UXKXGuaN7cBEVqIiC0sEaIbvzNhuqe4Wf7v97DDG\n2xRi36vNlkv16Hh4LR1kBu1+expIkx6lpZlJMwl2adBKJr1NtDFf2TVH6wKBgQCd\n/ZmGLkuYMVCTktxLxfeIMPECk1uvhrnbWQfK3Zgv+pAHdYI7hnZOoJP8L0fAJc2h\ny0pGZFPyOnMznY3ZWfc1HZHWux3bGJtyIbyxCFi/Y/dVlvoa/pObwSb1H0/PxC27\n7iTjDH3UWZKne9O1J5j17Ty6cEI6cKjFQBQ0LZgTXQKBgQCQMUmKV3IhppV+mkDC\ndqnJlxH7aSBgWF+dLzJJBc0Z04XpFFi8jpUaSF/aedO4BSlHBnJjWt0r86X6qINs\n8XjR0Mlmx0F7SZuqDLkzhFZCExiNZE/mzPTZ3P008xPCij8Nf0F8EUj0PodmgWZt\nyvJ2kxqw2YS57Vt8INZvxRjhTg==\n-----END PRIVATE KEY-----\n",
-  "client_email": "earth-engine-service-account@ee-abhisheksasidharanr.iam.gserviceaccount.com",
-  "client_id": "110619773534188793927",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/earth-engine-service-account%40ee-abhisheksasidharanr.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-}'''
-        if not service_account_json:
-            raise ValueError("GOOGLE_APPLICATION_CREDENTIALS environment variable not set.")
+        file_path = "credentials.json"
+        with open(file_path, "r") as file:
+            service_account_json = file.read()
+            if not service_account_json:
+                raise ValueError("GOOGLE_APPLICATION_CREDENTIALS environment variable not set.")
 
-        # Convert the JSON string to a dictionary
-        service_account_info = json.loads(service_account_json)
+                # Convert the JSON string to a dictionary
+                service_account_info = json.loads(service_account_json)
 
-        # Authenticate with Earth Engine using the service account
-        credentials = ee.ServiceAccountCredentials(email=None, key_data=json.dumps(service_account_info))
-        ee.Initialize(credentials)
+                # Authenticate with Earth Engine using the service account
+                credentials = ee.ServiceAccountCredentials(email=None, key_data=json.dumps(service_account_info))
+                ee.Initialize(credentials)
 
-        print("Earth Engine API initialized successfully!")
+                print("Earth Engine API initialized successfully!")
     except Exception as e:
         print(f"Error initializing Earth Engine API: {e}")
         raise e
